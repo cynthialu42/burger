0440e707fabd10a99f75888cc9b0fcf76dfdb82d
cynthialu42 <clu2334@gmail.com>
2018-05-20 15:05:46 -0500

    at last working needs style

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -18,33 +18,16 @@ router.post("/api/burgers", function(req, res){
     });
 });
 
-// router.put("api/burgers/:id", function(req, res){
-//     var condition = "id = " + req.params.id;
-//     console.log('here');
-//     burger.updateOne({devoured: req.body.devoured}, condition, function(data){
-//         console.log('here');
-//         if(data.changedRows == 0){
-//             return res.status(404).end();
-//         } else {
-//             res.status(200).end();
-//         }
-//     });
-// });
-
 router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    console.log("condition" + condition);
-  
+    var condition = "id = " + req.params.id;  
     burger.updateOne({
-      devoured: req.body.devoured
+        devoured: req.body.devoured
     }, condition, function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
     });
-  });
+});
 module.exports = router;
